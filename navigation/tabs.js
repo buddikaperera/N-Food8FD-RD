d97ff7ab98a6f94676a269bdbdb1265c875805f5
Buddhika Perera <buddhika.p@informaticsint.com>
2021-09-23 16:46:31 +0530

    navigation bottom tab all icons added

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -13,7 +13,7 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
      return (
-          <Tab.Navigator>
+          <Tab.Navigator tabBarOptions={{ showLabel: false }}>
                <Tab.Screen
                     name="Home"
                     component={Home}
@@ -23,8 +23,68 @@ const Tabs = () => {
                                    source={icons.cutlery}
                                    resizeMode="contain"
                                    style={{
-                                        width: 25,
-                                        height: 25,
+                                        width: 28,
+                                        height: 28,
+                                        tintColor: focused
+                                             ? COLORS.primary
+                                             : COLORS.secondary,
+                                   }}
+                              />
+                         ),
+                    }}
+               />
+
+               <Tab.Screen
+                    name="Search"
+                    component={Home}
+                    options={{
+                         tabBarIcon: ({ focused }) => (
+                              <Image
+                                   source={icons.search}
+                                   resizeMode="contain"
+                                   style={{
+                                        width: 28,
+                                        height: 28,
+                                        tintColor: focused
+                                             ? COLORS.primary
+                                             : COLORS.secondary,
+                                   }}
+                              />
+                         ),
+                    }}
+               />
+
+               <Tab.Screen
+                    name="Like"
+                    component={Home}
+                    options={{
+                         tabBarIcon: ({ focused }) => (
+                              <Image
+                                   source={icons.like}
+                                   resizeMode="contain"
+                                   style={{
+                                        width: 28,
+                                        height: 28,
+                                        tintColor: focused
+                                             ? COLORS.primary
+                                             : COLORS.secondary,
+                                   }}
+                              />
+                         ),
+                    }}
+               />
+
+               <Tab.Screen
+                    name="User"
+                    component={Home}
+                    options={{
+                         tabBarIcon: ({ focused }) => (
+                              <Image
+                                   source={icons.user}
+                                   resizeMode="contain"
+                                   style={{
+                                        width: 28,
+                                        height: 28,
                                         tintColor: focused
                                              ? COLORS.primary
                                              : COLORS.secondary,
