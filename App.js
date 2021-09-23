5c5d66aba608a8ad22e267cd566b3d10f1323bfb
Buddhika Perera <buddhika.p@informaticsint.com>
2021-09-23 15:49:14 +0530

    navigation &  stack created

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
+ *  <Stack.Screen name="Home" component={Tabs} />
  * @format
  * @flow strict-local
  */
 
 import React from "react";
 
-import {
-     SafeAreaView,
-     ScrollView,
-     StatusBar,
-     StyleSheet,
-     Text,
-     useColorScheme,
-     View,
-} from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { Restaurant, OrderDelivery } from "./screens";
+
+const Stack = createStackNavigator();
 
 const App = () => {
      return (
-          <SafeAreaView style={styles.container}>
-               <View>
-                    <Text>Sample Project</Text>
-               </View>
-          </SafeAreaView>
+          <NavigationContainer>
+               <Stack.Navigator
+                    screenOptions={{
+                         headerShown: false,
+                    }}
+                    initialRouteName={"Home"}
+               >
+                    <Stack.Screen name="Restaurant" component={Restaurant} />
+                    <Stack.Screen
+                         name="OrderDelivery"
+                         component={OrderDelivery}
+                    />
+               </Stack.Navigator>
+          </NavigationContainer>
      );
 };
 
